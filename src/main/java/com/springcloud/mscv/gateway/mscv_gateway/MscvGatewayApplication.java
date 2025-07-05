f1581eb6eb25d0d13ce2da7c97a3b11b1ce47caa
Diego Alejandro <diegoalejadro.solano@hotmail.com>
2025-07-05 14:26:31 -0600

    first commit

---
+++ src/main/java/com/springcloud/mscv/gateway/mscv_gateway/MscvGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.mscv.gateway.mscv_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscvGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscvGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springcloud/mscv/gateway/mscv_gateway/MscvGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.mscv.gateway.mscv_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscvGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
